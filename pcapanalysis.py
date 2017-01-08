663913cddfd8bcae09e4a645d83d86d76ceba2c0
root <fredbarrsmith@gmail.com>
2017-01-08 14:08:19 +0000

    opening files

--- pcapanalysis.py
+++ pcapanalysis.py
@@ -1,8 +1,17 @@
 #def main ():
 ##	iterator()
 
+
+
 #main()
-import dpkt, os
+import dpkt, os, scapy
+
+
+
+def pcapanalyser(file):
+	print file
+
 for file in os.listdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap"):
 	if file.endswith(".pcap"):
 		print(file)
+		pcapanalyser(file)
