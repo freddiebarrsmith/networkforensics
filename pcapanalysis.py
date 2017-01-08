96e4459ce2754f352785123605850d5f51e721a6
root <fredbarrsmith@gmail.com>
2017-01-08 14:58:20 +0000

    pcap now working now

--- pcapanalysis.py
+++ pcapanalysis.py
@@ -4,14 +4,18 @@
 
 
 #main()
-import dpkt, os, scapy
+#from scapy import *
+#import scapy
+#from scapy import *
+import scapy.all as scapy
 
+#def pcapanalyser(file):
+#	print file
+a = scapy.rdpcap("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap/feadef3e-a62e-47d6-8eec-697af9ac22f4.pcap")
+print(a)
+#	print a
 
-
-def pcapanalyser(file):
-	print file
-
-for file in os.listdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap"):
-	if file.endswith(".pcap"):
-		print(file)
-		pcapanalyser(file)
+#for file in os.listdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap"):
+#	if file.endswith(".pcap"):
+#		print(file)
+#		pcapanalyser(file)
