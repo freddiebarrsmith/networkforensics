34f87c69a9ac218ad8082399fc2efe957fa21902
root <fredbarrsmith@gmail.com>
2017-01-14 14:56:08 +0000

    adding object orientation to recognise hosts and packets

--- pcapanalysis.py
+++ pcapanalysis.py
@@ -1,21 +1,49 @@
 #def main ():
 ##	iterator()
 
+class Host:
+	#this is for identifying each host, their classification and it's behaviour
+	def __init__(self, ip):
+		self.ip = ip
+
+
+	def displayip(self):
+
+
+
+
+
 
 
 #main()
 #from scapy import *
 #import scapy
 #from scapy import *
+
+
+#class hosts
+
+
+##maybe want to classify know friendly hosts
+
+
+#class packets
+#maybe want to classify packets
+
+
+import os
 import scapy.all as scapy
 
-#def pcapanalyser(file):
-#	print file
-a = scapy.rdpcap("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap/feadef3e-a62e-47d6-8eec-697af9ac22f4.pcap")
-print(a)
+def pcapanalyser(file):
+	os.chdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap")
+	print file
+	a = scapy.rdpcap(file)
+	a.show()
+	print(a)
 #	print a
 
-#for file in os.listdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap"):
-#	if file.endswith(".pcap"):
-#		print(file)
-#		pcapanalyser(file)
+
+for file in os.listdir("/root/Desktop/mscdissforensics/pcaps/laredo-13.mit.edu/~brendan/regin/pcap"):
+	if file.endswith(".pcap"):
+		print(file)
+		pcapanalyser(file)
